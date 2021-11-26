03d0f78b8e8a56585fe3ca238e12bd64b8c42248
teang1995 <teang1995@ajou.ac.kr>
2021-11-26 13:59:20 +0900

    secrets.py ok

--- secrets.py
+++ secrets.py
@@ -8,3 +8,10 @@ db_password = os.getenv('DB_PASSWORD')
 db_host = os.getenv('DB_HOST')
 db_port = os.getenv('DB_PORT')
 db_database = os.getenv('DB_DATABASE')
+
+if __name__ == "__main__":
+    print(db_username)
+    print(db_password)
+    print(db_host)
+    print(db_port)
+    print(db_database)
